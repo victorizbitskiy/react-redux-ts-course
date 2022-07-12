63ff99c73c830c92a26e8386653c48e3e3280bfe
victorizbitskiy <victor.izbitskiy@yandex.ru>
2022-07-12 21:30:41 +0300

    reducer

---
+++ src/types/user.ts
@@ -0,0 +1,28 @@
+export enum UserActionTypes {
+  FETCH_USERS = 'FETCH_USERS',
+  FETCH_USERS_SUCCESS = 'FETCH_USERS',
+  FETCH_USERS_ERROR = 'FETCH_USERS',
+}
+
+export interface UserState {
+  users: any[];
+  loading: boolean;
+  error: null | string | any;
+}
+
+export interface UserAction {
+  type: UserActionTypes;
+  payload: any[];
+}
+
+// interface FetchUsersSuccessAction {
+//   type: UserActionTypes.FETCH_USERS_SUCCESS;
+//   payload: any[];
+// }
+
+// interface FetchUsersErrorAction {
+//   type: UserActionTypes.FETCH_USERS_ERROR;
+//   payload: string;
+// }
+
+// export type UserAction =  FetchUsersAction  | FetchUsersSuccessAction | FetchUsersErrorAction
